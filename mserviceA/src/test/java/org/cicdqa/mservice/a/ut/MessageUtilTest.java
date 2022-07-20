08a66c133b7f785196824eecede702317336b18f
RobertoSannino <rob.sannino@libero.it>
2022-07-20 14:59:51 +0200

    triggere analysis

--- mserviceA/src/test/java/org/cicdqa/mservice/a/ut/MessageUtilTest.java
+++ mserviceA/src/test/java/org/cicdqa/mservice/a/ut/MessageUtilTest.java
@@ -8,11 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MessageUtilTest {
 
+    private static final String SERVICE_NAME = "SERVICE_NAME";
+
     @Test
     void testHomeMessage() {
         Assertions.assertEquals(
             "*** Micro-Service SERVICE_NAME !!! ***",
-            MessageUtil.getHomeMessage("SERVICE_NAME")
+            MessageUtil.getHomeMessage(SERVICE_NAME)
         );
     }
 
