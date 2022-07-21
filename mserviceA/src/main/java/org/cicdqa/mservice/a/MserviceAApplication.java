e3329c47ae2fbcbf151ff47e0d8b8ab89139b7e8
marco.petrini <marco.petrini@larus-ba.it>
2022-07-21 17:11:55 +0200

    test commit

--- mserviceA/src/main/java/org/cicdqa/mservice/a/MserviceAApplication.java
+++ mserviceA/src/main/java/org/cicdqa/mservice/a/MserviceAApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MserviceAApplication {
 
 	public static void main(String[] args) {
-		//test
+		//test commit
 		SpringApplication.run(MserviceAApplication.class, args);
 	}
 
