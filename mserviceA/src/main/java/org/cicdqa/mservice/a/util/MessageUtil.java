e6e377d0e2da19a950623fc1555d84c21d2f0e87
RobertoSannino <rob.sannino@libero.it>
2022-08-01 15:44:30 +0200

    test PR that will break

--- mserviceA/src/main/java/org/cicdqa/mservice/a/util/MessageUtil.java
+++ mserviceA/src/main/java/org/cicdqa/mservice/a/util/MessageUtil.java
@@ -5,6 +5,6 @@ public class MessageUtil {
     private MessageUtil() {}
 
     public static String getHomeMessage(String serviceName) {
-        return  "*** Micro-Service " + serviceName + " !!! ***";
+        return  "*** UPDATED: Micro-Service " + serviceName + " !!! ***";
     }
 }
