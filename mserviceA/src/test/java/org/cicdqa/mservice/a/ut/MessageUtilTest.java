82f0312981d07dcfb7b9d9eebba62094fd318246
RobertoSannino <rob.sannino@libero.it>
2023-02-17 14:08:59 +0000

    added test for service

--- mserviceA/src/test/java/org/cicdqa/mservice/a/ut/MessageUtilTest.java
+++ mserviceA/src/test/java/org/cicdqa/mservice/a/ut/MessageUtilTest.java
@@ -9,12 +9,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MessageUtilTest {
 
     private static final String SERVICE_NAME = "SERVICE_NAME";
+    private static final String PERSON_NAME = "MARIO";
 
     @Test
     void testHomeMessage() {
         Assertions.assertEquals(
             "*** Micro-Service SERVICE_NAME !!! ***",
             MessageUtil.getHomeMessage(SERVICE_NAME)
+        );
+    }
+
+    @Test
+    void testHelloMessage() {
+        Assertions.assertEquals(
+                "Hello MARIO!",
+                MessageUtil.getHelloMessage(PERSON_NAME)
         );
     }
 
